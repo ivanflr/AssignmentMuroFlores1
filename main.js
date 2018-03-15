b77268278e19ab52c758c531c12f42934b0d5990
Adam K. Muro <bread@Adams-Air.home>
2018-03-15 19:32:19 -0400

    first audio

--- main.js
+++ main.js
@@ -4,10 +4,17 @@ $(document).ready(function () {
 
 		var clicked_spot = $(this);
 		var description = clicked_spot.data("description");
+		var audio = clicked_spot.data("audio");
 
-		$(".modal").find(".inner").find("p").html(description);
+
+
+		$(".modal").find(".inner").find("p").text(description);
+		$(".modal").find(".inner").find(".audio").html(audio);
 		$(".modal").addClass("active");
 
+
+
+			console.log(audio)
 	});
 	$(".close").on("click", function (e) {
 		e.preventDefault();
